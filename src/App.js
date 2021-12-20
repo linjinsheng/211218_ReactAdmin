21f0910c904cce004299d619e4893658430049cb
“linjinsheng” <linjinshengscut@126.com>
2021-12-20 11:57:45 +0800

    增加login和admin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react'
-import {Button, message} from 'antd'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 /**
  * 应用的根组件
  */
 export default class App extends Component {
  
-    handleClick = () => { 
-        message.success('成功啦...');
-    }
     
     render() { 
         return (
-            <Button type='primary' onClick={this.handleClick}>学习</Button> 
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/' component={Admin}/>
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
