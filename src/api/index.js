ab89d8e987237be8a8b666a310b134feee99716d
“linjinsheng” <linjinshengscut@126.com>
2021-12-21 22:51:51 +0800

    退出登录处理

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,31 @@
 import ajax from './ajax'
+import {message} from 'antd'
+import jsonp from 'jsonp'
 
 // const BASE = 'http://localhost:5000'
 const BASE = ''
 
 //  登陆
 export const reqLogin = (username, password) => ajax(BASE + '/login', {username, password}, 'POST');
+
+/**
+ * jsonp请求的接口请求函数
+ */
+export const reqWeather = (city) => {
+    return new Promise((resolve, reject) => {
+      const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+      // 发送jsonp请求
+      jsonp(url, {}, (err, data) => {
+        console.log('jsonp()', err, data)
+        // 如果成功了
+        if (!err && data.status==='success') {
+          // 取出需要的数据
+          const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+          resolve({dayPictureUrl, weather})
+        } else {
+          // 如果失败了
+          message.error('获取天气信息失败!')
+        }
+      })
+    })
+  }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,26 +1,118 @@
 import React, {Component} from 'react'
+import { Modal} from 'antd'
+import {withRouter} from 'react-router-dom'
+import LinkButton from '../link-button'
+import {reqWeather} from '../../api'
+import menuList from '../../config/menuConfig'
+import {formateDate} from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import storageUtils from '../../utils/storageUtils'
 import './index.less'
 
 /**
  * 头部组件
  */
-export default class Header extends Component {
+class Header extends Component {
+    state = {
+        currentTime: formateDate(Date.now()),   //  当前时间字符串
+        dayPictureUrl: '',                      //  天气图片url
+        weather: '',                            //  天气的文本
+    }
+
+    getTime = ()=>{
+        // 每隔1s获取当前时间, 并更新状态数据currentTime
+        this.intervalId = setInterval(() => {
+            const currentTime = formateDate(Date.now());
+            this.setState({currentTime});
+        }, 1000)
+    }
+
+    getWeather = async () => {
+        //  调用接口请求异步获取数据
+        const {dayPictureUrl, weather} = await reqWeather('北京');
+        //  更新状态
+        this.setState({dayPictureUrl, weather})
+    }
+
+    getTitle = () => {
+        //  得到当前请求路径
+        const path = this.props.location.pathname;
+        let title;
+        menuList.forEach(item => {
+            if(item.key === path) {
+                title = item.title;
+            } else if(item.children){
+                //  在所有的子item中查找匹配的
+                const cItem = item.children.find(cItem => cItem.key === path);
+                //  如果有值才说明有匹配的
+                if(cItem){
+                    title = cItem.title;
+                }
+            }
+        })
+        return title;
+    }
+
+    /**
+     * 退出登录
+     */
+    logout = () => {
+        //  显示确认框
+        Modal.confirm({
+            content: '确认退出吗?',
+            onOk: () => {
+                console.log('OK', this);
+                //  删除保存的user数据
+                storageUtils.removeUser();
+                memoryUtils.user = {};
+
+                //  跳转到login
+                this.props.history.replace('/login');
+            }
+        })
+    }
+
+    /*
+    第一次render()之后执行一次
+    一般在此执行异步操作: 发ajax请求/启动定时器
+    */
+    componentDidMount () {
+        //   获取当前的时间
+        this.getTime();
+        //   获取当前天气
+        this.getWeather()
+    }
+
+    /*
+      当前组件卸载之前调用
+    */
+   componentWillUnmount () {
+       // 清除定时器
+       clearInterval(this.intervalId)
+   }
+
+
     render() {
+        const {currentTime, dayPictureUrl, weather} = this.state;
+        const username = memoryUtils.user.username;
+        const title = this.getTitle();
         return(
             <div className="header">
                 <div className='header-top'>
-                    <span>欢迎,admin</span>
-                    <a href="javascript:">退出</a>
+                    <span>欢迎, {username}</span>
+                    <LinkButton onClick={this.logout}>退出</LinkButton>
                 </div>
                 <div className='header-bottom'>
-                    <div className="header-bottom-left">首页</div>
+                    <div className="header-bottom-left">{title}</div>
                     <div className="header-bottom-right">
-                        <span>2021-12-21 17:36:43</span>
-                        <img src='http://api.map.baidu.com/images/weather/day/qing.png' alt='weather'/>
-                        <span>晴</span>
+                        <span>{currentTime}</span>
+                        <img src={dayPictureUrl} alt='weather'/>
+                        <span>{weather}</span>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+export default  withRouter(Header) 
