bf01a1781754dffb90a1c6cad66c95a843715b4f
“linjinsheng” <linjinshengscut@126.com>
2021-12-22 21:14:22 +0800

    修改商品管理

--- src/api/index.js
+++ src/api/index.js
@@ -8,6 +8,35 @@ const BASE = ''
 //  登陆
 export const reqLogin = (username, password) => ajax(BASE + '/login', {username, password}, 'POST');
 
+// 获取一级/二级分类的列表
+export const reqCategorys = (parentId) => ajax(BASE + '/manage/category/list', {parentId})
+
+// 添加分类
+export const reqAddCategory = (categoryName, parentId) => ajax(BASE + '/manage/category/add', {categoryName, parentId}, 'POST')
+
+// 更新分类
+export const reqUpdateCategory = ({categoryId, categoryName}) => ajax(BASE + '/manage/category/update', {categoryId, categoryName}, 'POST')
+
+// 获取一个分类
+export const reqCategory = (categoryId) => ajax(BASE + '/manage/category/info', {categoryId})
+
+// 获取商品分页列表
+export const reqProducts = (pageNum, pageSize) => ajax(BASE + '/manage/product/list', {pageNum, pageSize})
+
+// 更新商品的状态(上架/下架)
+export const reqUpdateStatus = (productId, status) => ajax(BASE + '/manage/product/updateStatus', {productId, status}, 'POST')
+
+/*
+搜索商品分页列表 (根据商品名称/商品描述)
+searchType: 搜索的类型, productName/productDesc
+ */
+export const reqSearchProducts = ({pageNum, pageSize, searchName, searchType}) => ajax(BASE + '/manage/product/search', {
+    pageNum,
+    pageSize,
+    [searchType]: searchName,
+  })
+
+
 /**
  * jsonp请求的接口请求函数
  */
---
+++ src/pages/product/detail.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import {
+    Card,
+    Icon,
+    List
+} from 'antd'
+const Item = List.Item
+
+/**
+ * Product的详情子路由组件
+ */
+export default class ProductDetail extends Component {
+    render() {
+        return(
+            <div>
+                ProductDetail
+            </div>
+        )
+    }
+}
